b83e42ff51f5bf6e0e10e21adec90b4067c7dd4d
denis-koptev <koptevda@ya.ru>
2017-05-24 03:48:30 +0300

    Icons redesigned and other tiny features

--- app/src/main/java/com/tobaccofeed/tobaccofeed/MainActivity.java
+++ app/src/main/java/com/tobaccofeed/tobaccofeed/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        //navigationView.getMenu().getItem(0).setChecked(true);
+        navigationView.getMenu().getItem(0).setChecked(true);
         onNavigationItemSelected(navigationView.getMenu().findItem(R.id.nav_main));
     }
 
